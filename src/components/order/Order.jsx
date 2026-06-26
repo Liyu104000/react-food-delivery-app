991f682e81033f432cef6122c27924964e6bd0aa
Liyu <tanliyu10@gmail.com>
2026-06-26 11:32:28 +0800

    refactor the remaining component files from the pages folder to the component folder

---
+++ src/components/order/Order.jsx
@@ -0,0 +1,19 @@
+import { NavBar } from "../../shared/nav/Navbar";
+import { OrderHistory } from "../../pages/order/OrderHistory";
+
+
+export function Order() {
+  return (
+    <>
+      <title>My Orders | UrbanPlate</title>
+
+      <header>
+        <NavBar />
+      </header>
+
+      <main>
+        <OrderHistory/>
+      </main>
+    </>
+  );
+}
