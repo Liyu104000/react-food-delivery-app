91b965a0f1b4a3f15c310023138a36540e1e9839
Liyu <tanliyu10@gmail.com>
2026-07-21 14:03:49 +0800

    make delivery schedule dropdown interactive

--- src/pages/home/Dropdown.jsx
+++ src/pages/home/Dropdown.jsx
@@ -1,51 +1,173 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX, faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import dayjs from "dayjs";
 import "./Dropdown.css";
 
+const currentMinutesBaseline = dayjs().minute();
+const initialStart = dayjs().minute(currentMinutesBaseline < 30 ? 0 : 30).second(0);
+const initialTimeSlotLabel = `${initialStart.format("h:mmA")} - ${initialStart.add(30, "minute").format("h:mmA")}`;
+
 export function Dropdown() {
+ 
+  const calculateDeliveryDates = () => {
+    const deliveryDates = [];
+
+    for (let i = 1; i <= 5; i++) {
+      const formattedDeliveryDates = dayjs().add(i, "day").format("MMM D");
+      deliveryDates.push({
+        id: `${i}`,
+        label: formattedDeliveryDates,
+      });
+    }
+
+    return deliveryDates;
+  };
+
+  const calculateDeliveryTimes = selectedDate => {
+    const deliveryTimes = [];
+
+    let timeTracker;
+
+    const closingTime = dayjs().hour(16).minute(0).second(0);
+
+    if(selectedDate === "Today"){
+      const currentTime = dayjs();
+      const openingTime = dayjs().hour(9).minute(0).second(0);
+
+      if(currentTime.isBefore(openingTime)){
+        timeTracker = openingTime;
+      }else{
+        const currentMinutes = currentTime.minute();
+        timeTracker = dayjs().minute(currentMinutes < 30 ? 0 : 30).second(0);
+      }
+    }else{
+      timeTracker = dayjs().hour(9).minute(0).second(0);
+    }
+
+    for (let i = 1; i <= 6; i++) {
+      if(timeTracker.isAfter(closingTime) || timeTracker.isSame(closingTime)){
+        break;
+      }
+
+      const startTime = timeTracker.format("h:mmA");
+
+      if(selectedDate === "Today"){
+         timeTracker = timeTracker.add(30, "minute");
+      }else{
+         timeTracker = timeTracker.add(1, "hour");
+      }
+
+      const endTime = timeTracker.format("h:mmA");
+
+      deliveryTimes.push({
+        id: `${i}`,
+        label: `${startTime} - ${endTime}`,
+      });
+    }
+    return deliveryTimes;
+  };
+
+  const openDialog = () => {
+    setShowDialog(true);
+  };
+
+  const closeDialog = () => {
+    setTempDate(deliveryDate);
+    setTempTime(deliveryTime);
+    setShowDialog(false);
+  };
+
+  const handleConfirm = () => {
+    setDeliveryDate(tempDate);
+    setDeliveryTime(tempTime);
+    setShowDialog(false);
+  };
+
+  const [showDialog, setShowDialog] = useState(false);
+  const [deliveryDate, setDeliveryDate] = useState("Today");
+  const [deliveryTime, setDeliveryTime] = useState(initialTimeSlotLabel);
+  const [tempDate, setTempDate] = useState("Today");
+  const [tempTime, setTempTime] = useState(initialTimeSlotLabel);
+
+  const dynamicDeliveryDates = calculateDeliveryDates();
+  const dynamicDeliveryTimes = calculateDeliveryTimes(tempDate);
+
+  
+
   return (
     <section className="delivery-schedule-container">
       <div className="delivery-schedule-selector">
-        <span>Today . 2:00PM - 2:30PM</span>
+        <span>{dynamicDeliveryTimes.length > 0 ? `${deliveryDate} . ${deliveryTime}` : "UrbanPlate is closed"}</span>
 
-        <FontAwesomeIcon icon={faCaretDown} size="xl" color="rgb(51, 51, 51)" />
+        <FontAwesomeIcon
+          icon={faCaretDown}
+          size="xl"
+          color="rgb(51, 51, 51)"
+          onClick={openDialog}
+        />
       </div>
 
-      <div className="delivery-scedule-dialog">
-        <header className="delivery-schedule-header">
-          <h2 className="delivery-schedule-heading">When To Deliver?</h2>
+      {showDialog && (
+        <div className="delivery-scedule-dialog">
+          <header className="delivery-schedule-header">
+            <h2 className="delivery-schedule-heading">When To Deliver?</h2>
 
-          <FontAwesomeIcon
-            className="delivery-schedule-icon"
-            icon={faX}
-            color="rgb(51, 51, 51)"
-          />
-        </header>
+            <FontAwesomeIcon
+              className="delivery-schedule-icon"
+              icon={faX}
+              color="rgb(51, 51, 51)"
+              onClick={closeDialog}
+            />
+          </header>
 
-        <section className="date-options">
-          <button className="btn-delivery-date is-selected">Today</button>
-          <button className="btn-delivery-date">Feb 10</button>
-          <button className="btn-delivery-date">Feb 11</button>
-          <button className="btn-delivery-date">Feb 12</button>
-          <button className="btn-delivery-date">Feb 13</button>
-          <button className="btn-delivery-date">Feb 14</button>
-        </section>
+          <section className="date-options">
+            <button
+              className={`btn-delivery-date ${tempDate === "Today" ? "is-selected" : ""}`}
+              onClick={() => setTempDate("Today")}
+            >
+              Today
+            </button>
 
-        <header>
-          <h3 className="delivery-schedule-heading">At What Time?</h3>
-        </header>
+            {dynamicDeliveryDates.map((date) => (
+              <button
+                key={date.id}
+                className={`btn-delivery-date ${tempDate === date.label ? "is-selected" : ""}`}
+                onClick={() => setTempDate(date.label)}
+              >
+                {date.label}
+              </button>
+            ))}
+          </section>
 
-        <section className="time-options">
-          <button className="btn-delivery-time is-selected">2:00PM - 2:30PM</button>
-          <button className="btn-delivery-time">2:30PM - 3:00PM</button>
-          <button className="btn-delivery-time">3:00PM - 3:30PM</button>
-          <button className="btn-delivery-time">3:30PM - 4:00PM</button>
-        </section>
+          <header>
+            <h3 className="delivery-schedule-heading">At What Time?</h3>
+          </header>
 
-        <footer>
-          <button className="btn-success btn-confirm">Confirm</button>
-        </footer>
-      </div>
+          <section className="time-options">
+            {dynamicDeliveryTimes.length > 0 ? (
+              dynamicDeliveryTimes.map(time => (
+              <button
+                key={time.id}
+                className={`btn-delivery-time ${tempTime === time.label ? "is-selected" : ""}`}
+                onClick={() => setTempTime(time.label)}
+              >{time.label}</button>
+            ))
+            ) : (
+              <p className="closed-msg">UrbanPlate is closed for today. Order again tomorrow.</p>
+            )}
+            
+          </section>
+
+          <footer>
+            <button className={`btn-confirm ${dynamicDeliveryTimes.length === 0 ? "btn-success-disable" : "btn-success"}`} onClick={handleConfirm}
+             disabled = {dynamicDeliveryTimes.length === 0}
+            >
+              Confirm
+            </button>
+          </footer>
+        </div>
+      )}
     </section>
   );
 }
